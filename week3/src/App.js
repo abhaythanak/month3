a2c0a83941e37c8d668b5d92fc5cc65d13657805
Abhay Thanak <107474114+abhaythanak@users.noreply.github.com>
2022-10-16 20:36:35 +0530

    Add files via upload

---
+++ week3/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Nav from './component/nav';
+import Main from './component/main';
+import './App.css';
+
+function App() {
+  return (
+  <>
+  <Nav />
+  <Main />
+  </>
+  );
+}
+
+export default App;
---
+++ week3/src/component/data/data.js
@@ -0,0 +1,53 @@
+const data = {
+    productItems:[
+        {
+            id: "1",
+            name: "shoes",
+            Price: 599,
+            image: "./images/shoes.jpg",
+        },
+        {
+            id: "2",
+            name: "backCase",
+            Price: 299,
+            image: "./images/backCase.jpg",
+        },
+        {
+            id: "3",
+            name: "T-shirt",
+            Price: 999,
+            image: "./images/t-shirt.jpg",
+        },
+        {
+            id: "4",
+            name: "brown clutch",
+            Price: 699,
+            image: "./images/brown-clutch.jpg",
+        },
+        {
+            id: "5",
+            name: "watch",
+            Price: 199,
+            image: "./images/watches.jpg",
+        },
+        {
+            id: "6",
+            name: "iPhone14",
+            Price: 55599,
+            image: "./images/iphone.webp",
+        },
+        {
+            id: "7",
+            name: "Beats Headphone",
+            Price: 22599,
+            image: "./images/beats.webp",
+        },
+        {
+            id: "8",
+            name: "onePlus10T",
+            Price: 39499,
+            image: "./images/onePlus.webp",
+        },
+    ]
+}
+export default data;
